08ce5820e5e394b23d632ee2b022913c706171b8
AndreHuerto <diegohuertopeu.edu.pe>
2021-11-18 18:46:27 -0500

    tercer commit

--- src/config/default.js
+++ src/config/default.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 module.exports = {
     database : {
-        host :  `ec2-3-230-149-158.compute-1.amazonaws.com `,
+        host :  `ec2-3-230-149-158.compute-1.amazonaws.com`,
         user : `ihgvisybiwxiab`,
         password : `824a47437bb9939cfbd01913f0d93fc3a2e189f0c7242e2030a96703923b407b`,
         database : `d5jdkifp2osl55`,
